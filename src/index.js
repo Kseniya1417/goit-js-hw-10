b7e712930bfbf0280e163487e3ea23fce5c8cb42
Oksana Fylypchak <107584846+Kseniya1417@users.noreply.github.com>
2023-02-21 22:15:39 +0100

    update js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import fetchCountries from './js/fetchCountries';
+import renderCountries from './renderCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const listOfCountries = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.coutry-info');
+
+function searchCountries(e) {
+  let getCountry = e.target.value.trim();
+  // 1) тут викликається наша функція fetchCountries
+  if (getCountry) {
+    fetchCountries(getCountry)
+      .then(data => {
+        // 11) ми отримали б масив обєктів і попали б у then знову
+        // 12) і там би ми прийняли в then цю data (data -
+        // це наш response)
+        renderCountries(data);
+      })
+      // 9) якщо у нас error то ми then оминаємо і попадаємо
+      // зразу в catch
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+      });
+  } else {
+    listOfCountries.innerHTML = '';
+    countryInfo.innerHTML = '';
+  }
+}
+input.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
